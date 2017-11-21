157cc58f7fd14b981d9911641ff466f5aa2f955e
Chris Scott <chris.scott718@gmail.com>
2017-11-21 14:11:23 -0800

    add welcome message for new users and alert others

--- server/server.js
+++ server/server.js
@@ -16,15 +16,33 @@ let server = http.createServer(app);
 let io = socketIO(server);
 
 io.on('connection', (socket) => {
+
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app niggas...',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'A new user has joined',
+    createdAt: new Date().getTime()
+  });
+
   // emits to a single connection
-  console.log('New user connected');
   socket.on('createMessage', (message) => {
     // io aka socketIO(server) emits events to every single connection
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
-    })
+    });
+
+    // emits to everybody but this socket
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
 });
