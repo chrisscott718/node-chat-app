1d371f08cde93072c82a4308ad06a99de0cf0091
Chris Scott <chris.scott718@gmail.com>
2017-11-21 15:53:08 -0800

    Add jquery. add form. add message append on message submit.

--- public/js/index.js
+++ public/js/index.js
@@ -4,5 +4,19 @@ socket.on('connect', function() {
 });
 
 socket.on('newMessage', function(message) {
-  console.log('NEW MESSAGE RECEIVED', message);
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  $('#messages').append(li);
 })
+
+$('#message-form').on('submit', function(e){
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('input[name=message]').val()
+  }, function(data){
+    $('input[name=message]').val('').focus();
+  })
+});
--- server/server.js
+++ server/server.js
@@ -3,48 +3,28 @@ const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
 
-// set public path -- path is built into node to flatten directories/paths
-const publicPath = path.join(__dirname, '..', '/public');
+const {generateMessage} = require('./utils/message');
 
-// set port -- should be done in config file
-const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..', '/public'); // set public path -- path is built into node to flatten directories/paths
+const port = process.env.PORT || 3000; // set port -- should be done in config file
+let app = express(); // create server
+let server = http.createServer(app); // set up server for websockets
+let io = socketIO(server); // initialize websockets to receive connections
 
-// create server
-let app = express();
-
-let server = http.createServer(app);
-let io = socketIO(server);
-
+// when a user connects to the server
 io.on('connection', (socket) => {
-
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app niggas...',
-    createdAt: new Date().getTime()
-  });
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'A new user has joined',
-    createdAt: new Date().getTime()
-  });
-
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app.'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'A new user has joined.'));
   // emits to a single connection
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     // io aka socketIO(server) emits events to every single connection
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
-
-    // emits to everybody but this socket
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    // when the client emits something to the Server
+    // we can pass a call back with it.
+    // if things go well here we can acknowledge it by calling the callback
+    // which will be called on the client side. we can also send data back
+    callback('This is from the server.');
   });
-
 });
 
 // middleware to tell express to use this static directory
