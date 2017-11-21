e028391540398a203b9817fb2e104a04f58a0256
Chris Scott <chris.scott718@gmail.com>
2017-11-20 16:38:55 -0800

    add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 
 // set public path -- path is built into node to flatten directories/paths
 const publicPath = path.join(__dirname, '..', '/public');
@@ -10,16 +12,20 @@ const port = process.env.PORT || 3000;
 // create server
 let app = express();
 
+let server = http.createServer(app);
+let io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+  socket.on('disconnect', () => {
+    console.log('Disconnected from client');
+  });
+});
+
 // middleware to tell express to use this static directory
 app.use(express.static(publicPath));
 
-// route for base url
-// dont technically have to use this since it will default to index.html
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
 // have app listen for requests
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
 })
